02845f6b702f1802101023a01e1659f2654e1de8
Stanislav Smirnov <stanislav.ktak.smirnov@gmail.com>
2024-04-29 22:41:05 +0300

    Libs from coloring have been added

--- testproj2.nix
+++ testproj2.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, base, lib }:
+{ mkDerivation, base, bytestring, conduit, lib, parsec, rainbow
+, text
+}:
 mkDerivation {
   pname = "testproj2";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [
+    base bytestring conduit parsec rainbow text
+  ];
   license = lib.licenses.mit;
   mainProgram = "testproj2";
 }
